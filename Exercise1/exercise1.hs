fbccec3bb7a40febec145e77f855d0ff6d2209c0
Aasawaree Deshmukh <aasawaree.21090@gmail.com>
2018-01-29 10:13:00 -0500

    Haskell Exercise CIS 623 Syracuse

---
+++ Exercise1/exercise1.hs
@@ -0,0 +1,186 @@
+-- Name - Aasawaree Deshmukh
+-- SU ID - 98608 2803
+
+-- CIS 623, Exercises 1
+
+-- Problem 1 ----------------------------------------------------------
+
+-- degenerate a b c 
+--   tests whether the line ax+by+c=0 is degenerate
+-- EXAMPLES
+--   (degenerate 0 0 1) should return True
+--   (degenerate 1 1 0) should return False
+degenerate :: Float -> Float -> Float -> Bool
+degenerate a b c = if a == 0 && b == 0 then True else False 
+
+-- TESTS
+t2a = degenerate 0 0 1 -- should be True
+t2b = degenerate 1 1 0 -- should be False
+t2c = degenerate 3 0 1 -- should be False
+t2d = degenerate 0 1 0 -- should be False
+t2e = degenerate 0 0 7 -- should be True
+
+-- Problem 2 ----------------------------------------------------------
+
+-- onLine (x1,y1) a b c 
+--   tests whether (x1,y1) is on the line ax+by+c=0.
+-- EXAMPLES
+--   (onLine (1,1) 1 1 0) should return True
+--   (onLine (1,1) 1 1 1) should return False
+onLine :: (Float,Float) -> Float -> Float -> Float -> Bool
+onLine (x,y) a b c = if (a*x + b*y + c) == 0 then True else False 
+
+-- TESTS
+t1a = onLine (1,1) 1 1 0  -- should be False
+t1b = onLine (1,1) 1 1 1  -- should be False
+t1c = onLine (2,5) (-2) 1 (-1) -- should be True  
+
+-- Problem 3 -----------------------------------------------------------
+
+-- horizonal a b c 
+--   tests whether the line ax+by+c=0 is horizonal.
+--   We assume the line is nondegenerate.
+-- EXAMPLES
+--   (horizonal 0 (-3) 4) should be True
+--   (horizonal 1 1 12)   should be False
+horizonal ::  Float -> Float -> Float -> Bool
+horizonal a b c = if a == 0 && b /= 0 then True else False
+
+-- TESTS
+t3a = horizonal 0 (-3) 4 -- should be True
+t3b = horizonal 1 1 12   -- should be False
+t3c = horizonal 0 0 1    -- should be False
+-- Problem 4 -----------------------------------------------------------
+
+-- vertical a b c 
+--   tests whether the line ax+by+c=0 is vertical.
+--   We assume the line is nondegenerate.
+
+-- EXAMPLES
+--   (vertical 3 0 (-2)) should return True
+--   (vertical 1 1 12)   should return False
+
+vertical  ::  Float -> Float -> Float -> Bool
+vertical a b c = if b == 0 && a /= 0 then True else False -- <<<<< CHANGE THIS <<<<<
+
+-- TESTS
+t4a = vertical 3 0 (-2) -- should be True
+t4b = vertical 1 1 12   -- should be False
+t4c = vertical 0 0 1   -- should be False
+
+-- Problem 5 -----------------------------------------------------------
+
+-- xIntercept a b c 
+--   = the x-intercept of the line ax+by+c=0, if it has one.
+--   If the line is degenerate or horizonal, then 0.0 is returned.
+-- EXAMPLES
+--   (xIntercept 0 0 0) should return 0.0
+--   ****ADD MORE****
+xIntercept :: Float -> Float -> Float -> Float
+xIntercept a b c = if not ( degenerate a b c ) && not ( horizonal a b c ) then (-c/a) else 0.0
+
+-- TESTS
+t5a = xIntercept 0 0 0 -- should be 0.0
+t5b = xIntercept 1 4 1 -- should be -1.0
+t5c = xIntercept 2 3 1 -- should be -0.5
+t5d = xIntercept (-1) 2 2 -- should be 2.0
+
+-- Problem 6 -----------------------------------------------------------
+
+-- yIntercept a b c 
+--   = the y-intercept of the line ax+by+c=0, if it has one.
+--   If the line is degenerate or vertical, then 0.0 is returned.
+-- EXAMPLES
+--   (yIntercept 0 0 0) should return 0.0
+
+yIntercept :: Float -> Float -> Float -> Float
+yIntercept a b c = if not ( degenerate a b c ) && not ( vertical a b c ) then (-c/b) else 0.0 
+
+-- TESTS
+t6a = yIntercept 0 0 0  --  should be 0.0
+t6b = yIntercept 1 4 1 -- should be -0.25
+t6c = yIntercept 2 3 1 -- should be -0.33
+t6d = yIntercept (-1) (-2) 2 -- should be 1.0
+
+-- Problem 7 -----------------------------------------------------------
+
+-- parallel a1 b1 c1 a2 b2 c2 
+--   tests whether the two lines a1*x+b1*y+c1=0 and a2*x+b2*y+c2=0
+--   are parallel.  We assume neither line is degenerate.
+-- EXAMPLES
+--  (parallel 1 2 24 3 6 (-9)) should return True
+--   ****ADD MORE****
+
+-- DEFINITION
+parallel :: Float -> Float -> Float -> Float -> Float -> Float -> Bool
+parallel a1 b1 c1 a2 b2 c2 =  if (a1 * b2) == (a2 * b1) then True else False 
+
+-- TESTS
+t7a = parallel 1 2 24 3 6 (-9) -- should be True
+t7b = parallel 2 4 4 3 6 8 -- should be True
+t7c = parallel 1 0 0 3 6 4 -- should be False
+
+
+-- Problem 8 -----------------------------------------------------------
+
+-- intersect a1 b1 c1 a2 b2 c2 
+--  tests whether the two lines a1*x+b1*y+c1=0 and a2*x+b2*y+c2=0 intersect 
+--  in a single point.  If either of the two lines are degenerate, 
+--  then False is returned.
+-- EXAMPLES
+--   (intersect 1 1 0 (-1) 1 0) should be True
+
+intersect :: Float -> Float -> Float -> Float -> Float -> Float -> Bool
+intersect a1 b1 c1 a2 b2 c2 = if not (parallel a1 b1 c1 a2 b2 c2) then True else False
+
+-- TESTS
+t8a = intersect 1 1 0 (-1) 1 0 --  should be True
+t8b = intersect 2 4 4 3 6 8 -- should be False
+
+-- Problem 9 -----------------------------------------------------------
+
+-- intersectionPt a1 b1 c1 a2 b2 c2 
+--   determines the point of intersection the two lines
+--   a1*x+b1*y+c1=0 and a2*x+b2*y+c2=0, if they have one.
+--   If either line is degenerate or if the lines do not 
+--   intersect in a point, (0.0,0.0) is returned.
+-- EXAMPLES
+--   (intersectionPt 1 0 (-1) 0 1 (-1)) should return (1.0,1.0)
+
+intersectionPt :: Float -> Float -> Float -> Float -> Float 
+                  -> Float -> (Float,Float)  
+intersectionPt a1 b1 c1 a2 b2 c2 = if intersect a1 b1 c1 a2 b2 c2 then ((b1*c2 - b2*c1)/(a1*b2 - a2*b1) , (a2*c1 - a1*c2)/(a1*b2 - a2*b1)) else (0.0 , 0.0)
+
+-- TESTS
+-- The following is a helper function to do the tests here.
+-- Given a1, b1, c1, a2, b2, and c2, it finds the supposed
+-- intersection point via intersectionPt and then checks
+-- that the result really is on the two lines.  The ``where''
+-- business in simply one way of introducing local variables
+-- in Haskell.  We will cover it in detail later in the course.
+checkIntersect a1 b1 c1 a2 b2 c2 
+    = (onLine (x,y) a1 b1 c2) && (onLine (x,y) a2 b2 c2)
+      where (x,y) = intersectionPt a1 b1 c1 a2 b2 c2 
+
+t9a = checkIntersect 1 0 (-1) 0 1 (-1) -- should be True
+t9b = checkIntersect 1 1 (-2) (-1) 2 (-1) -- should be False
+
+-- Problem 10 ----------------------------------------------------------
+
+-- lineEqual a1 b1 c1 a2 b2 c2 
+--   tests whether the two lines a1*x+b1*y+c1=0 and  a2*x+b2*y+c2=0 are 
+--   nondegenerate and equal.
+-- EXAMPLES
+--   (lineEqual 1 1 1 2 2 2) should be True
+
+lineEqual :: Float -> Float -> Float -> Float -> Float -> Float -> Bool
+lineEqual a1 b1 c1 a2 b2 c2 = if parallel a1 b1 c1 a2 b2 c2 && ( (-c1/a1) == (-c2/a2) || (-c1/b1) == (-c2/b2)) then True else False
+
+-- TESTS
+t10a = lineEqual 1 1 1 2 2 2 -- should be True
+t10b = lineEqual 1 0 0 2 0 0 -- should be True
+t10c = lineEqual 0 1 0 0 2 0 -- should be True
+t10d = lineEqual 0 1 1 0 2 0 -- should be True
+
+------------------------------------------------------------------------
+------------------------------------------------------------------------
---
+++ Exercise2/exercise2.hs
@@ -0,0 +1,209 @@
+-- Name - Aasawaree Deshmukh
+-- SU ID - 98608 2803
+
+-- CIS 623, Exercises 2
+
+-- • Problem 1. ----------------------------------------------------------
+
+-- Write a function halveEvens :: [Int] -> [Int], which takes as argument a list of integers and returns half of each even integer in that list.
+-- For example halveEvens [0,2,1,7,8] == [0,1,4]
+
+-- Your definition should use list comprehension, not recursion.
+-- You may use the functions div, mod :: Int -> Int -> Int.
+
+-- • Problem 2. ----------------------------------------------------------
+
+-- Again, write a function which takes as argument a list of integers and returns half of each even integer in that list.
+-- For example halveEvensRec [0,2,1,7,8] == [0,1,4]
+
+-- This time use recursion, not a list comprehension. You may use div and mod again.
+
+-- • Problem 3. ----------------------------------------------------------
+
+-- Write a function inRange :: Int -> Int -> [Int] -> [Int] to return all numbers in the input list within the range given by the first two arguments (inclusive). For example,
+
+-- inRange 5 10 [1..15] == [5,6,7,8,9,10]
+
+-- Your definition should use list comprehension, not recursion.
+
+-- • Problem 4. ----------------------------------------------------------
+
+-- Once again, write a function to return all numbers in the input list within the range given by the first two arguments (inclusive). For example,
+
+-- inRangeRec 5 10 [1..15] == [5,6,7,8,9,10]
+
+-- This time your definition should use recursion, not list comprehension.
+
+
+-- • Problem 5. ----------------------------------------------------------
+
+-- Write a function countPositives to count the positive numbers (strictly greater than 0) in a list. For example,
+-- countPositives [0,1,-3,-2,8,-1,6] == 3
+-- Your solution should use a list comprehension. You may not use recursion, but you will need a specific library function.
+
+-- • Problem 6. ----------------------------------------------------------
+
+-- Again, write a function countPositivesRec to count the positive numbers (strictly greater than 0) in a list. For example,
+-- countPositivesRec [0,1,-3,-2,8,-1,6] == 3
+-- Your function countPositivesRec should use recursion and no library functions.
+-- Why do you think it's not possible to write countPositives using only list comprehension, without library functions?
+
+-- • Problem 7. ----------------------------------------------------------
+
+-- Professor Pennypincher will not buy anything if he has to pay more than 199.00 Pounds. But, as a member of the Generous Teachers Society, he gets a 10% discount on anything he buys. Write a function pennypincher :: [Int] -> Int that takes a list of prices and returns the total amount that Professor Pennypincher would have to pay, if he bought everything that was cheap enough for him.
+
+-- Prices should be represented in Pence, not Pounds, by integers. To deduct 10% off them, you will need to convert them into float first, using the function fromIntegral. To convert back to Int, you can use the function round, which rounds to the nearest integer. You can write a helper function discount :: Int -> Int to do this. Note that all your function definitions should come with a type signature.
+
+-- For example,
+-- pennypincher [4500, 19900, 22000, 39900] == 41760
+
+-- Your solution should use a list comprehension, and you may use a library function to do the additions for you.
+
+-- • Problem 8. ----------------------------------------------------------
+
+-- Again, Professor Pennypincher will not buy anything if he has to pay more than 199.00 Pounds. But, as a member of the Generous Teachers Society, he gets a 10% discount on anything he buys. Write a function pennypincherRec :: [Int] -> Int that takes a list of prices and returns the total amount that Professor Pennypincher would have to pay, if he bought everything that was cheap enough for him.
+
+-- Prices should be represented in Pence, not Pounds, by integers. To deduct 10% off them, you will need to convert them into float first, using the function fromIntegral. To convert back to Int, you can use the function round, which rounds to the nearest integer. You can write a helper function discount :: Int -> Int to do this. Note that all your function definitions should come with a type signature.
+
+-- For example, pennypincherRec [4500, 19900, 22000, 39900] == 41760
+
+-- This time your solution should use a recursion and no library functions.
+
+-- • Problem 9. ----------------------------------------------------------
+
+-- Write a function multDigits :: String -> Int that returns the product of all the digits in the input string. If there are no digits, your function should return 1. For example,
+
+-- multDigits "The time is 4:25" == 40
+-- multDigits "No digits here!" == 1
+
+-- Your definition should use list comprehension. You'll need a library function to determine if a character is a digit, one to convert a digit to an integer, and one to do the multiplication.
+
+-- • Problem 10. ----------------------------------------------------------
+
+-- Now write an equivalent function multDigitsRec that also returns the product of all the digits in the input string. If there are no digits, your function should return 1. For example,
+
+-- multDigits "The time is 4:25" == 40
+-- multDigits "No digits here!" == 1
+
+-- This time use recursion. You may use library functions that act on single characters or integers, but you may not use library functions that act on a list.
+
+-- • Problem 11. ----------------------------------------------------------
+
+-- Write a function capitalise :: String -> String which, given a word, capitalises it. That means that the first character should be made uppercase and any other letters should be made lowercase. For example,
+
+-- capitalise "edINBurgH" == "Edinburgh"
+
+-- Your definition should use a list comprehension and library functions toUpper and toLower that change the case of a character.
+
+
+-- • Problem 12. ----------------------------------------------------------
+
+-- Write another function, this time using recursion, capitaliseRec which, given a word, capitalises it. That means that the first character should be made uppercase and any other letters should be made lowercase. For example,
+
+-- capitaliseRec "edINBurgH" == "Edinburgh"
+
+-- You may need to write a helper function; of the helper function and the main function only one needs to be recursive. You must write a type signature for each function you write.
+
+
+-- • Problem 13. ----------------------------------------------------------
+
+-- Using the function capitalise from the previous problem (which you should explicitly copy into the workspace for this problem), write a function
+
+-- title :: [String] -> [String]
+
+-- which, given a list of words, capitalises them as a title should be capitalised. The proper capitalisation of a title (for our purposes) is as follows: The first word should be capitalised. Any other word should be capitalised if it is at least four letters long. For example,
+
+-- title ["tHe", "sOunD", "ANd", "thE", "FuRY"] == ["The", "Sound", "and", "the", "Fury"]
+
+-- Your function should use a list comprehension, and not recursion. Besides the capitalise function, you will probably need some other auxiliary functions. You must specify their type signature. You may use library functions that change the case of a character and the function length.
+
+-- • Problem 14. ----------------------------------------------------------
+
+-- Now write the equivalent function using a recursion.
+-- Using the function capitalise from the previous problem (which you should explicitly copy into the workspace for this problem), write a function
+
+-- titleRec :: [String] -> [String]
+
+-- which, given a list of words, capitalises them as a title should be capitalised. The proper capitalisation of a title (for our purposes) is as follows: The first word should be capitalised. Any other word should be capitalised if it is at least four letters long. For example,
+
+-- titleRec ["tHe", "sOunD", "ANd", "thE", "FuRY"] == ["The", "Sound", "and", "the", "Fury"]
+
+-- You may use capitaliseRec and any of its auxiliary functions (but need to explicitly copy them into the workspace for this problem). All functions should have a type signature.
+
+-- • Problem 15. ----------------------------------------------------------
+
+-- Dame Curious is a crossword enthusiast. She has a long list of words that might appear in a crossword puzzle, but she has trouble finding the ones that fit a slot. Write a function
+
+-- crosswordFind :: Char -> Int -> Int -> [String] -> [String]
+
+-- to help her. The expression
+
+-- crosswordFind letter inPosition len words
+
+-- should return all the items from words which (a) are of the given length and (b) have letter in the position inPosition. For example, if Curious is looking for seven-letter words that have 'k' in position 1, she can evaluate the expression:
+
+-- crosswordFind 'k' 1 7 ["funky", "fabulous", "kite", "icky", "ukelele"] 
+
+-- which returns ["ukelele"]. (Remember that we start counting with 0, so position 1 is the second position of a string.)
+
+-- Your definition should use a list comprehension. You may also use a library function which returns the nth element of a list, for argument n, and the function length.
+
+-- • Problem 16. ----------------------------------------------------------
+
+-- Write a recursive function crosswordFindRec to the same specification (you can use the same library functions).
+
+-- Dame Curious is a crossword enthusiast. She has a long list of words that might appear in a crossword puzzle, but she has trouble finding the ones that fit a slot. Write a function
+
+-- crosswordFindRec :: Char -> Int -> Int -> [String] -> [String]
+
+-- to help her. The expression
+
+-- crosswordFindRec letter inPosition len words
+
+-- should return all the items from words which (a) are of the given length and (b) have letter in the position inPosition. For example, if Curious is looking for seven-letter words that have 'k' in position 1, she can evaluate the expression:
+
+-- crosswordFindRec 'k' 1 7 ["funky", "fabulous", "kite", "icky", "ukelele"] 
+
+-- which returns ["ukelele"]. (Remember that we start counting with 0, so position 1 is the second position of a string.)
+
+-- Use recursion. You may also use a library function which returns the nth element of a list, for argument n, and the function length.
+
+-- • Problem 17. ----------------------------------------------------------
+
+-- Write a function search :: String -> Char -> [Int] that returns the positions of all occurrences of the second argument in the first. For example
+
+-- search "Bookshop" 'o' == [1,2,6]
+-- search "senselessness's" 's' == [0,3,7,8,11,12,14]
+
+-- You should use a list comprehension, not recursion. You may use the function zip :: [a] -> [b] -> [(a,b)], the function length :: [a] -> Int, and the term forms [m..n] and [m..].
+
+-- • Problem 18. ----------------------------------------------------------
+
+-- Write the recursive function searchRec. You may like to use an auxiliary function in your definition, but you shouldn't use any library functions.
+-- Your function should return the positions of all occurrences of the second argument in the first. For example
+
+-- searchRec "Bookshop" 'o' == [1,2,6]
+-- searchRec "senselessness's" 's' == [0,3,7,8,11,12,14]
+
+
+-- • Problem 19. ----------------------------------------------------------
+
+-- Write a function contains that takes two strings and returns True if the first contains the second as a substring. You can use the library function isPrefixOf, which returns True if the second string begins with the first string, and/or any other library function. For example,
+
+-- contains "United Kingdom" "King" == True
+-- contains "Appleton" "peon" == False
+-- contains "" "" == True
+
+-- Your definition should use a list comprehension, not recursion. A hint: you can use the library function drop to create a list of all possible suffixes (``last parts'') of a string. 
+
+
+-- • Problem 20. ----------------------------------------------------------
+
+-- Now write a recursive function containsRec that takes two strings and returns True if the first contains the second as a substring. You can use the library function isPrefixOf, which returns True if the second string begins with the first string, and/or any other library function. For example,
+
+-- containsRec "United Kingdom" "King" == True
+-- containsRec "Appleton" "peon" == False
+-- containsRec "" "" == True
+
+-- Pay attention to the last case of the above three.
+

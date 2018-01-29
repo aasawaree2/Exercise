6db5dbf8aff3ea955e038294266613fc8f94a497
Aasawaree Deshmukh <aasawaree.21090@gmail.com>
2018-01-29 15:21:31 -0500

    Solved few more problems in exercise 2

--- Exercise2/exercise2.hs
+++ Exercise2/exercise2.hs
@@ -2,6 +2,9 @@
 
 -- CIS 623, Exercises 2
 
+import Data.Char
+import Data.List
+
 -- • Problem 1. ----------------------------------------------------------
 
 -- Write a function halveEvens :: [Int] -> [Int], which takes as argument a list of integers and returns half of each even integer in that list.
@@ -11,7 +14,7 @@
 -- You may use the functions div, mod :: Int -> Int -> Int.
 
 halveEvens :: [Int] -> [Int]
-halveEvens xs = [ x `div ` 2 | x <- xs , even x]
+halveEvens xs = [ x `div` 2 | x <- xs , even x]
 
 -- • Problem 2. ----------------------------------------------------------
 
@@ -20,13 +23,21 @@ halveEvens xs = [ x `div ` 2 | x <- xs , even x]
 
 -- This time use recursion, not a list comprehension. You may use div and mod again.
 
+halveEvensRec :: [Int] -> [Int]
+halveEvensRec [] = []
+halveEvensRec (x:xs) 
+    | even x = x `div` 2 : halveEvensRec xs
+    | otherwise = halveEvensRec xs
+
 -- • Problem 3. ----------------------------------------------------------
 
--- Write a function inRange :: Int -> Int -> [Int] -> [Int] to return all numbers in the input list within the range given by the first two arguments (inclusive). For example,
-
--- inRange 5 10 [1..15] == [5,6,7,8,9,10]
+-- Write a function inRange :: Int -> Int -> [Int] -> [Int] to return all numbers in the input list within the range given 
+-- by the first two arguments (inclusive). For example, inRange 5 10 [1..15] == [5,6,7,8,9,10]
 
 -- Your definition should use list comprehension, not recursion.
+
+inRange :: Int -> Int -> [Int] -> [Int]
+inRange low high xs = [ x | x <- xs , low <= x && high >= x ]
 
 -- • Problem 4. ----------------------------------------------------------
 
@@ -36,12 +47,20 @@ halveEvens xs = [ x `div ` 2 | x <- xs , even x]
 
 -- This time your definition should use recursion, not list comprehension.
 
+inRangeRec :: Int -> Int -> [Int] -> [Int]
+inRangeRec low high [] = []
+inRangeRec low high (x:xs)
+    | low <= x && high >= x = x : inRangeRec low high xs
+    | otherwise = inRangeRec low high xs
 
 -- • Problem 5. ----------------------------------------------------------
 
 -- Write a function countPositives to count the positive numbers (strictly greater than 0) in a list. For example,
 -- countPositives [0,1,-3,-2,8,-1,6] == 3
 -- Your solution should use a list comprehension. You may not use recursion, but you will need a specific library function.
+
+countPositives :: [Int] -> Int
+countPositives xs = length [ x | x <- xs , x > 0 ] 
 
 -- • Problem 6. ----------------------------------------------------------
 
@@ -50,44 +69,84 @@ halveEvens xs = [ x `div ` 2 | x <- xs , even x]
 -- Your function countPositivesRec should use recursion and no library functions.
 -- Why do you think it's not possible to write countPositives using only list comprehension, without library functions?
 
+countPositivesRec :: [Int] -> Int
+countPositivesRec [] = 0
+countPositivesRec (x:xs)
+    | x > 0 = 1 + countPositivesRec xs
+    | otherwise = countPositivesRec xs
+
 -- • Problem 7. ----------------------------------------------------------
 
--- Professor Pennypincher will not buy anything if he has to pay more than 199.00 Pounds. But, as a member of the Generous Teachers Society, he gets a 10% discount on anything he buys. Write a function pennypincher :: [Int] -> Int that takes a list of prices and returns the total amount that Professor Pennypincher would have to pay, if he bought everything that was cheap enough for him.
+-- Professor Pennypincher will not buy anything if he has to pay more than 199.00 Pounds. But, as a member of the Generous Teachers Society, 
+-- he gets a 10% discount on anything he buys. Write a function pennypincher :: [Int] -> Int that takes a list of prices and returns the total 
+-- amount that Professor Pennypincher would have to pay, if he bought everything that was cheap enough for him.
 
--- Prices should be represented in Pence, not Pounds, by integers. To deduct 10% off them, you will need to convert them into float first, using the function fromIntegral. To convert back to Int, you can use the function round, which rounds to the nearest integer. You can write a helper function discount :: Int -> Int to do this. Note that all your function definitions should come with a type signature.
+-- Prices should be represented in Pence, not Pounds, by integers. To deduct 10% off them, you will need to convert them into float first, 
+-- using the function fromIntegral. To convert back to Int, you can use the function round, which rounds to the nearest integer. 
+--You can write a helper function discount :: Int -> Int to do this. Note that all your function definitions should come with a type signature.
 
 -- For example,
 -- pennypincher [4500, 19900, 22000, 39900] == 41760
 
 -- Your solution should use a list comprehension, and you may use a library function to do the additions for you.
 
+discount :: Int -> Int
+discount x = round ((fromIntegral x) * 0.9)
+
+pennypincher :: [Int] -> Int
+pennypincher xs = sum [ discount x | x <- xs , discount x <= 19900]
+
 -- • Problem 8. ----------------------------------------------------------
 
--- Again, Professor Pennypincher will not buy anything if he has to pay more than 199.00 Pounds. But, as a member of the Generous Teachers Society, he gets a 10% discount on anything he buys. Write a function pennypincherRec :: [Int] -> Int that takes a list of prices and returns the total amount that Professor Pennypincher would have to pay, if he bought everything that was cheap enough for him.
+-- Again, Professor Pennypincher will not buy anything if he has to pay more than 199.00 Pounds. But, as a member of the Generous Teachers Society,
+-- he gets a 10% discount on anything he buys. Write a function pennypincherRec :: [Int] -> Int that takes a list of prices and returns the 
+-- total amount that Professor Pennypincher would have to pay, if he bought everything that was cheap enough for him.
 
--- Prices should be represented in Pence, not Pounds, by integers. To deduct 10% off them, you will need to convert them into float first, using the function fromIntegral. To convert back to Int, you can use the function round, which rounds to the nearest integer. You can write a helper function discount :: Int -> Int to do this. Note that all your function definitions should come with a type signature.
+-- Prices should be represented in Pence, not Pounds, by integers. To deduct 10% off them, you will need to convert them into float first, 
+--using the function fromIntegral. To convert back to Int, you can use the function round, which rounds to the nearest integer. 
+--You can write a helper function discount :: Int -> Int to do this. Note that all your function definitions should come with a type signature.
 
 -- For example, pennypincherRec [4500, 19900, 22000, 39900] == 41760
 
 -- This time your solution should use a recursion and no library functions.
 
+pennypincherRec :: [Int] -> Int 
+pennypincherRec [] = 0
+pennypincherRec (x:xs)
+    | discount x <= 19900 = discount x + pennypincherRec xs
+    | otherwise = pennypincherRec xs
+
+
 -- • Problem 9. ----------------------------------------------------------
 
--- Write a function multDigits :: String -> Int that returns the product of all the digits in the input string. If there are no digits, your function should return 1. For example,
+-- Write a function multDigits :: String -> Int that returns the product of all the digits in the input string. If there are no digits, 
+-- your function should return 1. For example,
 
 -- multDigits "The time is 4:25" == 40
 -- multDigits "No digits here!" == 1
 
--- Your definition should use list comprehension. You'll need a library function to determine if a character is a digit, one to convert a digit to an integer, and one to do the multiplication.
+-- Your definition should use list comprehension. You'll need a library function to determine if a character is a digit, one to convert a
+-- digit to an integer, and one to do the multiplication.
+
+multDigits :: String -> Int
+multDigits s = product [ digitToInt x | x <- s , isDigit x ]
 
 -- • Problem 10. ----------------------------------------------------------
 
--- Now write an equivalent function multDigitsRec that also returns the product of all the digits in the input string. If there are no digits, your function should return 1. For example,
+-- Now write an equivalent function multDigitsRec that also returns the product of all the digits in the input string. 
+-- If there are no digits, your function should return 1. For example,
 
--- multDigits "The time is 4:25" == 40
--- multDigits "No digits here!" == 1
+-- multDigitsRec "The time is 4:25" == 40
+-- multDigitsRec "No digits here!" == 1
 
--- This time use recursion. You may use library functions that act on single characters or integers, but you may not use library functions that act on a list.
+-- This time use recursion. You may use library functions that act on single characters or integers, but you may not use library 
+-- functions that act on a list.
+
+multDigitsRec :: String -> Int
+multDigitsRec [] = 1
+multDigitsRec (x:xs)
+    | isDigit x = digitToInt x * multDigitsRec xs
+    | otherwise = multDigitsRec xs
 
 -- • Problem 11. ----------------------------------------------------------
 
